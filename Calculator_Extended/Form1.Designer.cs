cd32fa12700dcc61617dd22b5a3b42e952de2af2
AnnetteCarolyn <aneta.translation@gmail.com>
2025-03-22 16:52:19 +0100

    adding files

--- Calculator_Extended/Form1.Designer.cs
+++ Calculator_Extended/Form1.Designer.cs
@@ -28,187 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.SubtotalWorkingMinutesTextBox = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.workingDaysTextBox = new System.Windows.Forms.TextBox();
-            this.AdditionalOrMissingMinutesTextbox = new System.Windows.Forms.TextBox();
-            this.workingMinutesTextBox = new System.Windows.Forms.TextBox();
-            this.TotalResultTextBox = new System.Windows.Forms.TextBox();
-            this.tabControl1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
-            this.SuspendLayout();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            textBox1 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            label6 = new Label();
+            tabPage2 = new TabPage();
+            button2 = new Button();
+            label5 = new Label();
+            SubtotalWorkingMinutesTextBox = new TextBox();
+            button1 = new Button();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            workingDaysTextBox = new TextBox();
+            AdditionalOrMissingMinutesTextbox = new TextBox();
+            workingMinutesTextBox = new TextBox();
+            TotalResultTextBox = new TextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 3);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(792, 451);
-            this.tabControl1.TabIndex = 0;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(10, 2);
+            tabControl1.Margin = new Padding(3, 2, 3, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(720, 353);
+            tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(784, 418);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(numericUpDown1);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Margin = new Padding(3, 2, 3, 2);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3, 2, 3, 2);
+            tabPage1.Size = new Size(712, 325);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(36, 98);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(228, 23);
+            textBox1.TabIndex = 7;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(36, 52);
+            numericUpDown1.Margin = new Padding(3, 2, 3, 2);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(131, 23);
+            numericUpDown1.TabIndex = 6;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            numericUpDown1.KeyUp += numericUpDown1_KeyUp;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(20, 25);
+            label6.Name = "label6";
+            label6.Size = new Size(199, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Choose the amount of working days";
+            label6.Click += label6_Click;
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.button2);
-            this.tabPage2.Controls.Add(this.label5);
-            this.tabPage2.Controls.Add(this.SubtotalWorkingMinutesTextBox);
-            this.tabPage2.Controls.Add(this.button1);
-            this.tabPage2.Controls.Add(this.label4);
-            this.tabPage2.Controls.Add(this.label3);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Controls.Add(this.workingDaysTextBox);
-            this.tabPage2.Controls.Add(this.AdditionalOrMissingMinutesTextbox);
-            this.tabPage2.Controls.Add(this.workingMinutesTextBox);
-            this.tabPage2.Controls.Add(this.TotalResultTextBox);
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(784, 418);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(SubtotalWorkingMinutesTextBox);
+            tabPage2.Controls.Add(button1);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(label2);
+            tabPage2.Controls.Add(label1);
+            tabPage2.Controls.Add(workingDaysTextBox);
+            tabPage2.Controls.Add(AdditionalOrMissingMinutesTextbox);
+            tabPage2.Controls.Add(workingMinutesTextBox);
+            tabPage2.Controls.Add(TotalResultTextBox);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Margin = new Padding(3, 2, 3, 2);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3, 2, 3, 2);
+            tabPage2.Size = new Size(712, 325);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            tabPage2.Click += tabPage2_Click;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(252, 314);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 29);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Add/Subtract";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            button2.Location = new Point(339, 230);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 22);
+            button2.TabIndex = 11;
+            button2.Text = "Add/Subtract";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(108, 184);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 20);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Subtotal";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
+            label5.AutoSize = true;
+            label5.Location = new Point(94, 138);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Subtotal";
             // 
             // SubtotalWorkingMinutesTextBox
             // 
-            this.SubtotalWorkingMinutesTextBox.Location = new System.Drawing.Point(104, 224);
-            this.SubtotalWorkingMinutesTextBox.Name = "SubtotalWorkingMinutesTextBox";
-            this.SubtotalWorkingMinutesTextBox.Size = new System.Drawing.Size(125, 27);
-            this.SubtotalWorkingMinutesTextBox.TabIndex = 9;
-            this.SubtotalWorkingMinutesTextBox.TextChanged += new System.EventHandler(this.SubtotalWorkingMinutes_TextChanged);
+            SubtotalWorkingMinutesTextBox.Location = new Point(91, 168);
+            SubtotalWorkingMinutesTextBox.Margin = new Padding(3, 2, 3, 2);
+            SubtotalWorkingMinutesTextBox.Name = "SubtotalWorkingMinutesTextBox";
+            SubtotalWorkingMinutesTextBox.Size = new Size(110, 23);
+            SubtotalWorkingMinutesTextBox.TabIndex = 9;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(252, 224);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Count!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Location = new Point(221, 169);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(82, 22);
+            button1.TabIndex = 8;
+            button1.Text = "Count!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(107, 346);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(103, 20);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "TOTAL RESULT";
+            label4.AutoSize = true;
+            label4.Location = new Point(94, 260);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 15);
+            label4.TabIndex = 7;
+            label4.Text = "TOTAL RESULT";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(104, 282);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 20);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Additional +/-";
+            label3.AutoSize = true;
+            label3.Location = new Point(91, 212);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Additional +/-";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(107, 111);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "x 480 minutes";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            label2.AutoSize = true;
+            label2.Location = new Point(94, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 15);
+            label2.TabIndex = 5;
+            label2.Text = "x 480 minutes";
+            label2.Click += label2_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(104, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(229, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Type the amount of working days";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            label1.AutoSize = true;
+            label1.Location = new Point(91, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(184, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Type the amount of working days";
+            label1.Click += label1_Click;
             // 
             // workingDaysTextBox
             // 
-            this.workingDaysTextBox.Location = new System.Drawing.Point(104, 64);
-            this.workingDaysTextBox.Name = "workingDaysTextBox";
-            this.workingDaysTextBox.Size = new System.Drawing.Size(242, 27);
-            this.workingDaysTextBox.TabIndex = 3;
+            workingDaysTextBox.Location = new Point(91, 48);
+            workingDaysTextBox.Margin = new Padding(3, 2, 3, 2);
+            workingDaysTextBox.Name = "workingDaysTextBox";
+            workingDaysTextBox.Size = new Size(212, 23);
+            workingDaysTextBox.TabIndex = 3;
             // 
             // AdditionalOrMissingMinutesTextbox
             // 
-            this.AdditionalOrMissingMinutesTextbox.Location = new System.Drawing.Point(107, 316);
-            this.AdditionalOrMissingMinutesTextbox.Name = "AdditionalOrMissingMinutesTextbox";
-            this.AdditionalOrMissingMinutesTextbox.Size = new System.Drawing.Size(125, 27);
-            this.AdditionalOrMissingMinutesTextbox.TabIndex = 2;
-            this.AdditionalOrMissingMinutesTextbox.TextChanged += new System.EventHandler(this.AdditionalOrMissingMinutesTextbox_TextChanged);
+            AdditionalOrMissingMinutesTextbox.Location = new Point(94, 229);
+            AdditionalOrMissingMinutesTextbox.Margin = new Padding(3, 2, 3, 2);
+            AdditionalOrMissingMinutesTextbox.Name = "AdditionalOrMissingMinutesTextbox";
+            AdditionalOrMissingMinutesTextbox.Size = new Size(239, 23);
+            AdditionalOrMissingMinutesTextbox.TabIndex = 2;
             // 
             // workingMinutesTextBox
             // 
-            this.workingMinutesTextBox.Location = new System.Drawing.Point(107, 134);
-            this.workingMinutesTextBox.Name = "workingMinutesTextBox";
-            this.workingMinutesTextBox.Size = new System.Drawing.Size(68, 27);
-            this.workingMinutesTextBox.TabIndex = 1;
-            this.workingMinutesTextBox.Text = "480";
+            workingMinutesTextBox.Location = new Point(94, 100);
+            workingMinutesTextBox.Margin = new Padding(3, 2, 3, 2);
+            workingMinutesTextBox.Name = "workingMinutesTextBox";
+            workingMinutesTextBox.Size = new Size(60, 23);
+            workingMinutesTextBox.TabIndex = 1;
+            workingMinutesTextBox.Text = "480";
             // 
             // TotalResultTextBox
             // 
-            this.TotalResultTextBox.Location = new System.Drawing.Point(104, 385);
-            this.TotalResultTextBox.Name = "TotalResultTextBox";
-            this.TotalResultTextBox.Size = new System.Drawing.Size(125, 27);
-            this.TotalResultTextBox.TabIndex = 0;
+            TotalResultTextBox.Location = new Point(91, 289);
+            TotalResultTextBox.Margin = new Padding(3, 2, 3, 2);
+            TotalResultTextBox.Name = "TotalResultTextBox";
+            TotalResultTextBox.Size = new Size(110, 23);
+            TotalResultTextBox.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 488);
-            this.Controls.Add(this.tabControl1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(752, 366);
+            Controls.Add(tabControl1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
@@ -229,5 +277,8 @@
         private TextBox textBox2;
         private TextBox SubtotalWorkingMinutesTextBox;
         private Button button2;
+        private NumericUpDown numericUpDown1;
+        private Label label6;
+        private TextBox textBox1;
     }
 }
--- Calculator_Extended/Form1.cs
+++ Calculator_Extended/Form1.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 namespace Calculator_Extended
 {
     public partial class Form1 : Form
@@ -24,54 +26,84 @@ namespace Calculator_Extended
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
             {
-              
-                    var workingDaysText = workingDaysTextBox.Text;
-                    var minutesOfWorkInADayText = workingMinutesTextBox.Text;
+                var workingDaysText = workingDaysTextBox.Text;
+                var minutesOfWorkInADayText = workingMinutesTextBox.Text;
 
-                    var workingDays = decimal.Parse(workingDaysText);
-                    var minutesOfWorkInADay = decimal.Parse(minutesOfWorkInADayText);
+                var workingDays = decimal.Parse(workingDaysText);
+                var minutesOfWorkInADay = decimal.Parse(minutesOfWorkInADayText);
 
-                    if (workingDays < 0)
-                    {
-                        workingDaysTextBox.Text = "Type a positive number";
-                    }
-                    else
-                    {
-                        var result = workingDays * minutesOfWorkInADay;
-
-                     SubtotalWorkingMinutesTextBox.Text = result.ToString();
-                    }
+                if (workingDays < 0)
+                {
+                    workingDaysTextBox.Text = "Type a positive number";
                 }
+                else
+                {
+                    var result = workingDays * minutesOfWorkInADay;
+
+                    SubtotalWorkingMinutesTextBox.Text = result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                workingDaysTextBox.Text = "Only numbers allowed";
+
+
+            }
+
+
         }
 
-        private void label5_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void SubtotalWorkingMinutes_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void button2_Click(object sender, EventArgs e)
         {
-           
-            var subtotalWorkingMinutes = decimal.Parse(SubtotalWorkingMinutesTextBox.Text);
-            var additionalOrMissingMinutes = decimal.Parse(AdditionalOrMissingMinutesTextbox.Text);
-            var totalResult = subtotalWorkingMinutes + additionalOrMissingMinutes;
+            try
+            {
 
-          
+                var subtotalWorkingMinutes = decimal.Parse(SubtotalWorkingMinutesTextBox.Text);
+                var additionalOrMissingMinutes = decimal.Parse(AdditionalOrMissingMinutesTextbox.Text);
+                var totalResult = subtotalWorkingMinutes + additionalOrMissingMinutes;
 
-            TotalResultTextBox.Text = totalResult.ToString();
+                TotalResultTextBox.Text = totalResult.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                AdditionalOrMissingMinutesTextbox.Text = "Write a positive or negative number";
+
+            }
         }
 
-        private void AdditionalOrMissingMinutesTextbox_TextChanged(object sender, EventArgs e)
+
+        private void label6_Click(object sender, EventArgs e)
         {
 
         }
-    }
 
-     
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //var workingDays = decimal.Parse(numericUpDown1_ValueChanged);
+            //var minutesOfWorkInADay = decimal.Parse(numericUpDown1_ValueChanged);
+            //var result = WorkingDays * 480;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            calculateTotalMinutes();
+        }
+
+        private void numericUpDown1_KeyUp(object sender, KeyEventArgs e)
+        {
+            calculateTotalMinutes();
+        }
+        private void calculateTotalMinutes()
+        {
+            var workingDays = numericUpDown1.Value;
+            var result = workingDays * 480;
+            textBox1.Text = "The number of working minutes is: " + result.ToString();
+        }
+
     }
+}
